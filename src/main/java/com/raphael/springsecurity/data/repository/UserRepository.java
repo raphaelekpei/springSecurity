148681df57ada079482c13a315b043372c30fad6
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-21 22:32:43 +0100

    Commit 110027

---
+++ src/main/java/com/raphael/springsecurity/data/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.raphael.springsecurity.data.repository;
+
+import com.raphael.springsecurity.data.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
