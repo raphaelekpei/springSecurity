99dff2965dd5fff11677caffde8b2ebb39522bf2
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-21 22:29:46 +0100

    Commit 110021

---
+++ src/main/java/com/raphael/springsecurity/dto/response/RegistrationResponse.java
@@ -0,0 +1,12 @@
+package com.raphael.springsecurity.dto.response;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class RegistrationResponse {
+    private String message;
+}
